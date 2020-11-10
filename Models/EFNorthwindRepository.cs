cc45b1b758a4dba776190ee55e2089f8f0ae4586
jgrissom <jgrissom91@gmail.com>
2020-11-10 09:41:26 -0600

    initial commit

---
+++ Models/EFNorthwindRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Northwind.Models
+{
+    public class EFNorthwindRepository : INorthwindRepository
+    {
+        // the repository class depends on the BloggingContext service
+        // which was registered at application startup
+        private NorthwindContext context;
+        public EFNorthwindRepository(NorthwindContext ctx)
+        {
+            context = ctx;
+        }
+        // create IQueryable for Blogs & Posts
+        public IQueryable<Category> Categories => context.Categories;
+        public IQueryable<Product> Products => context.Products;
+        public IQueryable<Discount> Discounts => context.Discounts;
+        public IQueryable<Customer> Customers => context.Customers;
+
+        public void AddCustomer(Customer customer)
+        {
+            context.Customers.Add(customer);
+            context.SaveChanges();
+        }
+
+        public void EditCustomer(Customer customer)
+        {
+            var customerToUpdate = context.Customers.FirstOrDefault(c => c.CustomerId == customer.CustomerId);
+            customerToUpdate.Address = customer.Address;
+            customerToUpdate.City = customer.City;
+            customerToUpdate.Region = customer.Region;
+            customerToUpdate.PostalCode = customer.PostalCode;
+            customerToUpdate.Country = customer.Country;
+            customerToUpdate.Phone = customer.Phone;
+            customerToUpdate.Fax = customer.Fax;
+            context.SaveChanges();
+        }
+    }
+}
